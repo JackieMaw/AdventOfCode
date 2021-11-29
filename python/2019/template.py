5517a6f37b9ae95ff37df7b220c6e5e8e95b39a3
JackieMaw <jackiemaw@gmail.com>
2021-11-29 14:44:34 +0000

    updated template

--- python/2019/template.py
+++ python/2019/template.py
@@ -7,12 +7,15 @@ import copy
 def execute(input):
     return 0
 
+# TESTS
 assert execute(get_strings_csv(["ABCD"])) == 0
 print("ALL TESTS PASSED")
 
+# REAL INPUT DATA
 YEAR = 2019
 DAY = 4
 raw_input = get_or_download_input(YEAR, DAY)
 input = get_strings_csv(raw_input)
+print(execute(input))
 assert execute(input) == 0
 print("ANSWER CORRECT")
