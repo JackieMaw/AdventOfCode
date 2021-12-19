39dede4dc343a04a920abfa4ef60e2d08ec59cdd
JackieMaw <jackiemaw@gmail.com>
2021-12-19 11:38:51 +0000

    AoC 2021 Day 17a Completed

--- python/2021day17a.py
+++ python/2021day17a.py
@@ -16,11 +16,13 @@ def move_next(pos, velocity):
     new_pos = (x + vx, y + vy)
     # because of drag, x movement is slowing down
     # because of gravity, y movement is slowing down if it's going up, or speeding up if it's going down 
-    new_velocity = (vx - math.copysign(1, vx), vy - 1)
-    #print(f"       MOVE - Position: {pos} => {new_pos}, Velocity: {velocity} => {new_velocity}")
+    vx = vx - abs(vx) / vx if vx != 0 else 0
+    vy = vy - 1
+    new_velocity = (vx, vy)
+    # print(f"       MOVE - Position: {pos} => {new_pos}, Velocity: {velocity} => {new_velocity}")
     return new_pos, new_velocity
 
-def hit_target(pos, target):
+def check_hit_target(pos, target):
     (x, y) = pos
     ((x_min, x_max), (y_min, y_max)) = target
     in_range = x >= x_min and x <= x_max and y >= y_min and y <= y_max
@@ -36,7 +38,7 @@ def will_hit_target(velocity, target):
     assert vx > 0
     while True:
         pos, velocity = move_next(pos, velocity)
-        in_range, missed = hit_target(pos, target) 
+        in_range, missed = check_hit_target(pos, target) 
         if in_range:
             print(f"HIT: velocity => {starting_velocity}")
             return True
@@ -44,52 +46,69 @@ def will_hit_target(velocity, target):
             print(f"MISSED: velocity => {starting_velocity}")
             return False    
 
-def execute(x_min, x_max, y_min, y_max):
-    max_vy = 0
-
-    target = ((x_min, x_max), (y_min, y_max))
+def get_good_vx(target):
     
-    # pick initial velocity which hits the target in the first step, by firing down
-    vx = x_min
-    vy = y_max
+    ((x_min, x_max), (y_min, y_max)) = target
+    # find the vx which has a vx_sum within the target range of x:
+    vx = 0
+    missed = False
+    while not missed:
+        vx += 1
+        vx_sum = vx * (vx + 1) / 2
+        in_range = vx_sum >= x_min and vx_sum <= x_max
+        missed = vx_sum > x_max
+        if in_range:
+            return vx
+
+    return None
+
+def execute(x_min, x_max, y_min, y_max):
+
+    max_vy = 0
+    target = ((x_min, x_max), (y_min, y_max))
+  
+    # pick initial velocity which hits the target in the first step, by firing up
+    vx = get_good_vx(target)
+    # picking this vy means vx will be 0 by the time the ball comes back to 0
+    vy = vx / 2
+    
     velocity = (vx, vy)
     print(f"Choose initial velocity {velocity} for target area: {target}")
+
     hit_target = will_hit_target(velocity, target)
     if hit_target and vy > max_vy:
-        print(f"New max vy {max_vy}")
         max_vy = vy
+        print(f"New max vy {max_vy}")
 
     # since vy has a direct correlation to the max_height, 
-    # increase vy until we miss 
-    # then decrease vx until we hit
-    # keep going while vx > 0
-
-    while vx > 1:
-        
-        while hit_target: # keep increasing vy until we miss
-            vy += 1
-            print(f"...increasing vy to {vy}")
-            velocity = (vx, vy)
-            hit_target = will_hit_target(velocity, target)
-            if hit_target and vy > max_vy:
-                print(f"New max vy {max_vy}")
-                max_vy = vy
-
-        vx -= 1 # keep decreasing vx until we hit
-        print(f"...decreasing vx to {vx}")
-        velocity = (vx, vy)        
+    # increase vy until we miss - will this ever happen?
+    while hit_target: # keep increasing vy until we miss
+        vy += 1
+        # print(f"...increasing vy to {vy}")
+        velocity = (vx, vy)
         hit_target = will_hit_target(velocity, target)
         if hit_target and vy > max_vy:
-            print(f"New max vy {max_vy}")
             max_vy = vy
+            # print(f"New max vy {max_vy}")
+
+    # try a few more times, just to see
+    for _ in range(1000):
+        vy += 1
+        # print(f"...increasing vy to {vy}")
+        velocity = (vx, vy)
+        hit_target = will_hit_target(velocity, target)
+        if hit_target and vy > max_vy:
+            max_vy = vy
+            # print(f"New max vy {max_vy}")
 
     result = max_vy * (max_vy + 1) / 2
     print(f"result: {result}") 
     return result
 
 # TESTS
-# assert execute(get_strings_csv(["ABCD"])) == 0
-# print("ALL TESTS PASSED")
+assert get_good_vx(((20, 30), (-10, -5))) == 6
+assert get_good_vx(((207, 263) , (-115, -63))) == 20
+print("ALL TESTS PASSED")
 
 YEAR = 2021
 DAY = 17
@@ -105,5 +124,5 @@ print("TEST INPUT PASSED")
 # raw_input = get_or_download_input(YEAR, DAY)
 # input = get_strings(raw_input)
 # target area: x=207..263, y=-115..-63
-assert execute(207, 263, -115, -63) == 0
+#assert execute(207, 263, -115, -63) == 0
 print("ANSWER CORRECT")
