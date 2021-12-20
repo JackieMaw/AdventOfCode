5dc0dab04a16e0c4892a725b1d541644063b4673
JackieMaw <jackiemaw@gmail.com>
2021-12-20 06:21:01 +0000

    AoC 2021 Day 20a Completed

--- python/2021day20a.py
+++ python/2021day20a.py
@@ -22,20 +22,23 @@ def parse_input(input):
     print()
     return image_lookup, image
 
-def get_image_key(x, y, image):
+def get_image_key(x, y, image, x_min, x_max, y_min, y_max, unexplored_value):
 
     key = ""
 
     for j in range(y-1, y+2):
         for i in range(x-1, x+2):
-            if (i, j) in image:
+            unexplored = i < x_min or i > x_max or j < y_min or j > y_max
+            if unexplored:
+                key += unexplored_value
+            elif (i, j) in image:
                 key += "1"
             else:                
                 key += "0"
     
     return int(key, 2)
 
-def play(image_lookup, image):
+def play(image_lookup, image, unexplored_value):
 
     xs = [ x for (x, y) in image] 
     x_min = min(xs)
@@ -45,14 +48,14 @@ def play(image_lookup, image):
     y_min = min(ys)
     y_max = max(ys)
     
-    print(f"PLAY: {x_min - 1}  => {x_max + 2}, {y_min - 1}  => {y_max + 2}")
+    print(f"PLAY: {x_min - 1}  => {x_max + 1}, {y_min - 1}  => {y_max + 1}")
 
     new_image = set()
     print()
     for y in range(y_min - 1, y_max + 2):
         line = ""
         for x in range(x_min - 1, x_max + 2):
-            key = get_image_key(x, y, image)
+            key = get_image_key(x, y, image, x_min, x_max, y_min, y_max, unexplored_value)
             pixel = image_lookup[key]
             line += pixel
             if pixel == "#":
@@ -67,8 +70,13 @@ def execute(input):
 
     print(f"Before any rounds, image has {len(image)} pixels")
 
+    alternating = image_lookup[0] == "#" and image_lookup[-1] == "."
+    if alternating:
+        print(f"WARNING: FLASHING DETECTED")
+
     for i in range(2):
-        image = play(image_lookup, image)
+        unexplored_value = "1" if alternating and i % 2 == 1 else "0"
+        image = play(image_lookup, image, unexplored_value)
         print(f"After round {i}, image has {len(image)} pixels")
 
     result = len(image)
@@ -91,5 +99,5 @@ print("TEST INPUT PASSED")
 # REAL INPUT DATA
 raw_input = get_or_download_input(YEAR, DAY)
 input = get_strings(raw_input)
-assert execute(input) == 0 # not 5469
+assert execute(input) == 5400
 print("ANSWER CORRECT")
