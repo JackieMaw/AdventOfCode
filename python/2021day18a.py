3d0f8b889dcebf6b01b5d877adc00cff7b9cd027
JackieMaw <jackiemaw@gmail.com>
2021-12-21 19:57:29 +0000

    AoC 2021 Day 15 In Progress - BROKEN TEST

--- python/2021day18a.py
+++ python/2021day18a.py
@@ -36,34 +36,81 @@ class PairSnailFishNumber(SnailFishNumber):
     def get_magnitude(self):
         return self.left.get_magnitude() * 3 + self.right.get_magnitude() * 2
 
-def add_simple(number1, number2):
+def parse_snailfish_literal(number):
+    return LiteralSnailFishNumber(int(number))
+
+def parse_snailfish_number(number):
+    # [1,2]
+    # [[[[1,2]]],[[[3,4]]]]
+
+    if number[0] != "[":
+        return parse_snailfish_literal(number)
+
+    # split the string into left and right, ignoring the first and last char
+    nesting = 0
+    for ptr in range(1, len(number) - 1):
+        if number[ptr] == "[":
+            nesting += 1
+        elif number[ptr] == "]":
+            nesting -= 1
+        elif number[ptr] == "," and nesting == 0:
+            # SPLIT HERE
+            break
+
+    s1 = number[1:ptr]
+    s2 = number[ptr+1:-1]
+
+    left = parse_snailfish_number(s1)
+    right = parse_snailfish_number(s2)
+
+    return PairSnailFishNumber(left, right)
+
+def add(number1, number2):
     new_number = f"[{number1},{number2}]"
-    print(f"add_simple {number1} + {number2} = {new_number}")
+    #print(f"add {number1} + {number2} = {new_number}")
     return new_number
 
 def explode_at(number, ptr):
-    # number[ptr] = "["
-    left_number = int(number[ptr+1])
-    right_number = int(number[ptr+3])
+
+    # number[ptr] = "[11,8]"
+    # number[ptr] = "[5,777]"
+
+    left_ptr_start = ptr + 1
+    left_ptr_end = left_ptr_start + 1
+    while left_ptr_end < len(number) and number[left_ptr_end] not in ["[", "]", ","]:
+        left_ptr_end += 1
+    left_number = int(number[left_ptr_start:left_ptr_end])
+
+    right_ptr_start = left_ptr_end + 1
+    right_ptr_end = right_ptr_start + 1
+    while right_ptr_end < len(number) and number[right_ptr_end] not in ["[", "]", ","]:
+        right_ptr_end += 1
+    right_number = int(number[right_ptr_start:right_ptr_end])
 
     s1 = number[:ptr]
-    s2 = number[ptr+5:]
+    s2 = number[right_ptr_end+1:]
 
     new_left_ptr = len(s1) - 1
     while new_left_ptr > 0 and s1[new_left_ptr] in ["[", "]", ","]:
         new_left_ptr -= 1
 
     if new_left_ptr > 0:
-        new_left_number = left_number + int(s1[new_left_ptr])
-        s1 = s1[:new_left_ptr] + str(new_left_number) + s1[new_left_ptr+1:]
+        new_left_ptr_end = new_left_ptr + 1
+        while new_left_ptr_end < len(s1) and s1[new_left_ptr_end] not in ["[", "]", ","]:
+            new_left_ptr_end += 1
+        new_left_number = left_number + int(s1[new_left_ptr:new_left_ptr_end])
+        s1 = s1[:new_left_ptr] + str(new_left_number) + s1[new_left_ptr_end:]
 
     new_right_ptr = 0
     while new_right_ptr < len(s2) and s2[new_right_ptr] in ["[", "]", ","]:
         new_right_ptr += 1
 
     if new_right_ptr < len(s2):
-        new_right_number = right_number + int(s2[new_right_ptr])
-        s2 = s2[:new_right_ptr] + str(new_right_number) + s2[new_right_ptr+1:]
+        new_right_ptr_end = new_right_ptr + 1
+        while new_right_ptr_end < len(s2) and s2[new_right_ptr_end] not in ["[", "]", ","]:
+            new_right_ptr_end += 1
+        new_right_number = right_number + int(s2[new_right_ptr:new_right_ptr_end])
+        s2 = s2[:new_right_ptr] + str(new_right_number) + s2[new_right_ptr_end:]
        
     return s1 + "0" + s2
 
@@ -77,7 +124,7 @@ def explode(number):
             nesting -= 1
         if nesting == 5:
             exploded_number = explode_at(number, ptr)
-            print(f"explode {number} => {exploded_number}")
+            #print(f"explode {number} => {exploded_number}")
             return exploded_number, True
 
     return number, False
@@ -104,7 +151,7 @@ def split(number):
     for ptr in range(len(number) - 1):
         if number[ptr] not in ["[", "]", ","] and number[ptr + 1] not in ["[", "]", ","]:
             split_number = split_at(number, ptr)
-            print(f"split {number} => {split_number}")
+            #print(f"split {number} => {split_number}")
             return split_number, True
 
     return number, False
@@ -126,12 +173,20 @@ def add_list(numbers):
         if result is None:
             result = number
         else:
-            result = add_simple(result, number)
-            result = reduce(result)
+            new_result = add(result, number)
+            result_reduced = reduce(new_result)
+            print(f"  {result}")
+            print(f"+ {number}")
+            print(f"= {result_reduced}")
+            print()
+            result = result_reduced
     return result
 
 def get_magnitude(number):
-    return 0
+
+    snail_fish_number = parse_snailfish_number(number)
+
+    return snail_fish_number.get_magnitude()
 
 def execute(input):
     print(input)
@@ -150,19 +205,17 @@ numberB = PairSnailFishNumber(LiteralSnailFishNumber(1), LiteralSnailFishNumber(
 assert numberB.get_magnitude() == 21
 assert PairSnailFishNumber(numberA, numberB).get_magnitude() == 129
 
-assert add_simple("[1,2]","[[3,4],5]") == "[[1,2],[[3,4],5]]"
+assert add("[1,2]","[[3,4],5]") == "[[1,2],[[3,4],5]]"
 assert explode("[[[[[9,8],1],2],3],4]") == ("[[[[0,9],2],3],4]", True)
 assert explode("[7,[6,[5,[4,[3,2]]]]]") == ("[7,[6,[5,[7,0]]]]", True)
 assert explode("[[6,[5,[4,[3,2]]]],1]") == ("[[6,[5,[7,0]]],3]", True)
 assert explode("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]") == ("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", True)
 assert explode("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]") == ("[[3,[2,[8,0]]],[9,[5,[7,0]]]]", True)
 assert split("[11,5]") == ("[[5,6],5]", True)
-assert add_simple("[[[[4,3],4],4],[7,[[8,4],9]]]","[1,1]") == "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"
+assert add("[[[[4,3],4],4],[7,[[8,4],9]]]","[1,1]") == "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"
 assert reduce("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]") == "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
 
-assert get_magnitude("[9,1]") == 29
-assert get_magnitude("[1,9]") == 21
-assert get_magnitude("[[9,1],[1,9]]") == 129
+assert explode("[[[[[109,108],21],2],3],4]") == ("[[[[0,129],2],3],4]", True)
 
 print("ALL TESTS PASSED")
 
@@ -170,20 +223,28 @@ YEAR = 2021
 DAY = 18
 
 # TEST INPUT DATA
-raw_input = get_input(YEAR, DAY, "_test1")
-input = get_strings(raw_input)
-assert add_list(input) == "[[[[1,1],[2,2]],[3,3]],[4,4]]"
-print("TEST INPUT 1 PASSED")
+# raw_input = get_input(YEAR, DAY, "_test1")
+# input = get_strings(raw_input)
+# assert add_list(input) == "[[[[1,1],[2,2]],[3,3]],[4,4]]"
+# print("TEST INPUT 1 PASSED")
 
-raw_input = get_input(YEAR, DAY, "_test2")
-input = get_strings(raw_input)
-assert add_list(input) == "[[[[3,0],[5,3]],[4,4]],[5,5]]"
-print("TEST INPUT 2 PASSED")
+# raw_input = get_input(YEAR, DAY, "_test2")
+# input = get_strings(raw_input)
+# assert add_list(input) == "[[[[3,0],[5,3]],[4,4]],[5,5]]"
+# print("TEST INPUT 2 PASSED")
 
-raw_input = get_input(YEAR, DAY, "_test3")
-input = get_strings(raw_input)
-assert add_list(input) == "[[[[5,0],[7,4]],[5,5]],[6,6]]"
-print("TEST INPUT 3 PASSED")
+# raw_input = get_input(YEAR, DAY, "_test3")
+# input = get_strings(raw_input)
+# assert add_list(input) == "[[[[5,0],[7,4]],[5,5]],[6,6]]"
+# print("TEST INPUT 3 PASSED")
+
+# assert get_magnitude("[9,1]") == 29
+# assert get_magnitude("[1,9]") == 21
+# assert get_magnitude("[[9,1],[1,9]]") == 129
+
+# breaking test!
+test4_line7 = ["[[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]", "[2,9]"]
+assert add_list(test4_line7) == "[[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]]"
 
 raw_input = get_input(YEAR, DAY, "_test4")
 input = get_strings(raw_input)
