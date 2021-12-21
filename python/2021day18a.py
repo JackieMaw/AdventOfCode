5198ffcf69331eee10712fd8cffde13a73a11af9
JackieMaw <jackiemaw@gmail.com>
2021-12-21 17:01:55 +0000

    AoC 2021 Day 15 In Progress

--- python/2021day18a.py
+++ python/2021day18a.py
@@ -6,22 +6,46 @@ import copy
 
 def execute(input):
     print(input)
-    result = 0
+
+    number = add_list(input)
+
+    result = get_magniture(number)
     print(f"result: {result}") 
     return result
 
 # TESTS
-# assert execute(get_strings_csv(["ABCD"])) == 0
-# print("ALL TESTS PASSED")
+assert add("[1,2]","[[3,4],5]") == "[[1,2],[[3,4],5]]"
+assert reduce("[[[[[9,8],1],2],3],4]") == "[[[[0,9],2],3],4]"
+assert reduce("[7,[6,[5,[4,[3,2]]]]]") == "[7,[6,[5,[7,0]]]]"
+assert reduce("[[6,[5,[4,[3,2]]]],1]") == "[[6,[5,[7,0]]],3]"
+assert reduce("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]") == "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
+assert add("[[[[4,3],4],4],[7,[[8,4],9]]]","[1,1]") == "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]"
+assert reduce("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]") == "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
+print("ALL TESTS PASSED")
 
 YEAR = 2021
 DAY = 18
 
 # TEST INPUT DATA
-# raw_input = get_input(YEAR, DAY, "_test")
-# input = get_strings(raw_input)
-# assert execute(input) == 0
-# print("TEST INPUT PASSED")
+raw_input = get_input(YEAR, DAY, "_test1")
+input = get_strings(raw_input)
+assert add_list(input) == "[[[[1,1],[2,2]],[3,3]],[4,4]]"
+print("TEST INPUT 1 PASSED")
+
+raw_input = get_input(YEAR, DAY, "_test2")
+input = get_strings(raw_input)
+assert add_list(input) == "[[[[3,0],[5,3]],[4,4]],[5,5]]"
+print("TEST INPUT 2 PASSED")
+
+raw_input = get_input(YEAR, DAY, "_test2")
+input = get_strings(raw_input)
+assert add_list(input) == "[[[[3,0],[5,3]],[4,4]],[5,5]]"
+print("TEST INPUT 2 PASSED")
+
+raw_input = get_input(YEAR, DAY, "_test2")
+input = get_strings(raw_input)
+assert add_list(input) == "[[[[3,0],[5,3]],[4,4]],[5,5]]"
+print("TEST INPUT 2 PASSED")
 
 # REAL INPUT DATA
 raw_input = get_or_download_input(YEAR, DAY)
