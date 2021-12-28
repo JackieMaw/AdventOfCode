f6bc73ef061f3edec5afff0ae804f34ed16ef3c4
JackieMaw <jackiemaw@gmail.com>
2021-12-28 22:42:11 +0200

    AoC 2021 Day 23a IN PROGRESS - Parsed Input and Computed Possible Moves

--- python/2021day23a.py
+++ python/2021day23a.py
@@ -5,33 +5,119 @@ import math
 import copy
 
 
+team_map = {"A" : 0, "B" : 1, "C" : 2, "D" : 3} 
+team_number_map = ["A", "B", "C", "D"]
 
-def get_possible_moves():
-    possible_moves = []
+def get_players(input_data):
 
-    # 1. Are there any players in the top of a room which can go home now?
-    # 
-    # Once an amphipod stops moving in the hallway, it will stay in that spot until it can move into a room
-    # Amphipods will never move from the hallway into a room unless that room is their destination room 
-    #   and that room contains no amphipods which do not also have that room as their own destination.
+    players = [ [] for _ in range(4) ]
 
-    # 2. Are there any players in the hallway which can go home?
-    # 
-    # Once an amphipod stops moving in the hallway, it will stay in that spot until it can move into a room
-    # Amphipods will never move from the hallway into a room unless that room is their destination room 
-    #   and that room contains no amphipods which do not also have that room as their own destination.
+    width = len(input_data[0])
+    length = len(input_data)
+
+    for x in range(width):
+        for y in range(length):
+            player = input_data[y][x]
+            if player in team_map:
+                index = team_map[player]
+                players[index].append((x, y))
+
+    print(players)
+
+    return players
+
+final_state = [[(3, 2), (3, 3)], [(5, 2), (5, 3)], [(7, 2), (7, 3)], [(9, 2), (9, 3)]]
+hallway = [(1, 1), (2, 1), (4, 1), (6, 1), (8, 1), (10, 1)]
+
+def get_settled(state):
+    return []
+
+def try_move_to(start, end, state, player_cost):
     
-    # 2. For each room, does the top player in the room need to move? There are 7 possible spaces in the hallway it can move to.
-    # 
-    # Amphipods will never stop on the space immediately outside any room.
-    # Amphipods will never move from the hallway into a room unless that room is their destination room 
-    #   and that room contains no amphipods which do not also have that room as their own destination.
+    players = [player for team_state in state for player in team_state]
+    # check if the destination is free
+    if end in players:
+        #print(f"  Cannot move from {start} to {end} because another player is already there.")
+        return None
+
+    # try to move to the destination one step at a time
+    (start_x, start_y) = start
+    (end_x, end_y) = end
+
+    #print(f"  Try move from {start} to {end} ==>")
+
+    # MOVES CAN BE CACHED, there are only 19 x 19 possible combinations
+
+    # first move up to the hallway from start_y to 1
+    moves = [(start_x, y) for y in range(start_y - 1, 0, -1)]
+
+    # then move across the hallway from start_x to end_x
+    if start_x < end_x:
+        moves.extend([(x, 1) for x in range(start_x + 1, end_x)])
+    elif start_x > end_x:
+        moves.extend([(x, 1) for x in range(start_x - 1, end_x, -1)])
+
+    # then move down into the room from 1 to end_y
+    moves.extend([(end_x, y) for y in range(1, end_y)])
+
+    for move in moves:
+        if move in players:
+            #print(f"     Checking {move}... BLOCKED")
+            return None
+        #else:        
+            #print(f"     Checking {move}... OK")
+
+    return (len(moves) + 1) * player_cost
+
+def get_possible_moves(state):
+
+    # players = list of locations for A, B, C, D players
+    #players = [[(5, 2), (5, 3)], [(3, 3), (7, 3)], [(3, 2), (9, 3)], [(7, 2), (9, 2)]]
+
+    possible_moves = []
+    settled = get_settled(state)
+
+    for team_number in range(4):
+        team_state = state[team_number]
+        team_name = team_number_map[team_number]
+
+        for player in team_state:
+            
+            print(f"Computing all possible moves for {team_name} @ {player}:")
+
+            # if the player is already home it will not move again
+            if player not in settled:
+
+                player_cost = 10 ** team_number
+
+                # try to move home 
+                home_room = final_state[team_number]
+                for home_space in home_room:
+                    cost = try_move_to(player, home_space, state, player_cost)
+                    if cost is not None:
+                        # TODO - also check no foreigners are in the room!
+                        possible_moves.append((player, home_space, cost))       
+                        print(f"*** {team_name} can move from {player} to {home_space} [HOME] with cost {cost}")
+                        # if you can move home, then all other moves are WORSE
+                        # return possible_moves    
+
+                # if you are not in the hallway already, try to move into hallway
+                if player not in hallway:
+                    for hallway_space in hallway:
+                        cost = try_move_to(player, hallway_space, state, player_cost)
+                        if cost is not None:
+                            possible_moves.append((player, hallway_space, cost))
+                            print(f"*** {team_name} can move from {player} to {hallway_space} [HALLWAY] with cost {cost}")
 
     return possible_moves
 
-def execute(input):
-    print(input)
-    result = 0
+def execute(input_data):
+    print(input_data)
+
+    players = get_players(input_data)
+    possible_moves = get_possible_moves(players)
+
+    result = len(possible_moves)
     print(f"result: {result}") 
     return result
 
@@ -43,10 +129,10 @@ YEAR = 2021
 DAY = 23
 
 # TEST INPUT DATA
-# raw_input = get_input(YEAR, DAY, "_test")
-# input = get_strings(raw_input)
-# assert execute(input) == 0
-# print("TEST INPUT PASSED")
+raw_input = get_input(YEAR, DAY, "_test")
+input = get_strings(raw_input)
+assert execute(input) == 12521
+print("TEST INPUT PASSED")
 
 # REAL INPUT DATA
 raw_input = get_or_download_input(YEAR, DAY)
--- python/utilities.py
+++ python/utilities.py
@@ -9,12 +9,12 @@ def get_or_download_input(year, day):
     if not(exists(filename)):
         download_input(year, day, filename)    
     with open(filename, "r") as text_file:
-        return [l.strip() for l in text_file.readlines()]
+        return text_file.read().splitlines()
 
 def get_input(year, day, postfix):
     filename = f"input\Input_{year}_{day}{postfix}.txt"
     with open(filename, "r") as text_file:
-        return [l.strip() for l in text_file.readlines()]
+        return text_file.read().splitlines()
 
 def download_input(year, day, filename):
     uri = f'http://adventofcode.com/{year}/day/{day}/input'
@@ -24,19 +24,19 @@ def download_input(year, day, filename):
     print(f"input downloaded: {filename}")
 
 def get_strings(raw_input):
-    return [l.strip() for l in raw_input]
+    return [l for l in raw_input]
 
 def get_strings_csv(raw_input):
     result = []
     for line in raw_input:
         inner_result = []
         for l in line.split(","):   
-            inner_result.append(l.strip())
+            inner_result.append(l)
         result.append(inner_result)
     return result
 
 def get_integers(raw_input):
-    return [int(l.strip()) for l in raw_input]
+    return [int(l) for l in raw_input]
 
 def get_integers_csv(raw_input):
-    return [int(l.strip()) for l in raw_input[0].split(",")]
+    return [int(l) for l in raw_input[0].split(",")]
