484a9502416e4fc4038b6ff504fd5b31fbdc4d68
JackieMaw <jackiemaw@gmail.com>
2022-07-13 21:37:19 +0100

    cleaning for Advent of Code

--- python/2021day19b.py
+++ python/2021day19b.py
@@ -9,9 +9,10 @@ class Cube():
         self.name = name
         self.beacons = beacons
 
-def add_to_space(tile, offset, space):
-    print(f"Added {tile[0]} to space with offset: {offset}")
-    for beacon in tile[1]:
+def add_to_space(cube, offset, space):
+    (cube_name, beacons) = cube
+    #print(f"Added {cube_name} to space with offset: {offset}")
+    for beacon in beacons:
         (x, y, z) = beacon
         (ox, oy, oz) = offset
         beacon_in_space = (x + ox, y + oy, z + oz)
@@ -37,54 +38,49 @@ def parse_input(input):
 
     # this cube is completed
     cubes.append((f"Cube #{cube_number}", beacons))    
-    print(f"Loaded Last Cube #{cube_number}")
+    #print(f"Loaded Last Cube #{cube_number}")
 
     return cubes
 
-# CHEAT!
+# I totally cheated here!
+# I spent hours trying to do the rotations logically but couldn't get it right so I gave up
 
-transformation_functions = []
-transformation_functions.append(lambda x, y, z: (x, y, z))
-
-transformation_functions.append(lambda x, y, z: (x, z, -y))
-transformation_functions.append(lambda x, y, z: (-z, x, -y))
-transformation_functions.append(lambda x, y, z: (-x, -z, -y))
-
-transformation_functions.append(lambda x, y, z: (z, -x, -y))
-transformation_functions.append(lambda x, y, z: (z, -y, x))
-transformation_functions.append(lambda x, y, z: (y, z, x))
-
-transformation_functions.append(lambda x, y, z: (-z, y, x))
-transformation_functions.append(lambda x, y, z: (-y, -z, x))
-transformation_functions.append(lambda x, y, z: (-y, x, z))
-
-transformation_functions.append(lambda x, y, z: (-x, -y, z))
-transformation_functions.append(lambda x, y, z: (y, -x, z))
-
-transformation_functions.append(lambda x, y, z: (-z, -x, y))
-transformation_functions.append(lambda x, y, z: (x, -z, y))
-transformation_functions.append(lambda x, y, z: (z, x, y))
-
-transformation_functions.append(lambda x, y, z: (-x, z, y))
-transformation_functions.append(lambda x, y, z: (-x, y, -z))
-transformation_functions.append(lambda x, y, z: (-y, -x, -z))
-
-transformation_functions.append(lambda x, y, z: (x, -y, -z))
-transformation_functions.append(lambda x, y, z: (y, x, -z))
-transformation_functions.append(lambda x, y, z: (y, -z, -x))
-
-transformation_functions.append(lambda x, y, z: (z, y, -x))
-transformation_functions.append(lambda x, y, z: (-y, z, -x))
-transformation_functions.append(lambda x, y, z: (-z, -y, -x))
+rotation_functions = []
+rotation_functions.append(lambda x, y, z: (x, y, z))
+rotation_functions.append(lambda x, y, z: (x, z, -y))
+rotation_functions.append(lambda x, y, z: (-z, x, -y))
+rotation_functions.append(lambda x, y, z: (-x, -z, -y))
+rotation_functions.append(lambda x, y, z: (z, -x, -y))
+rotation_functions.append(lambda x, y, z: (z, -y, x))
+rotation_functions.append(lambda x, y, z: (y, z, x))
+rotation_functions.append(lambda x, y, z: (-z, y, x))
+rotation_functions.append(lambda x, y, z: (-y, -z, x))
+rotation_functions.append(lambda x, y, z: (-y, x, z))
+rotation_functions.append(lambda x, y, z: (-x, -y, z))
+rotation_functions.append(lambda x, y, z: (y, -x, z))
+rotation_functions.append(lambda x, y, z: (-z, -x, y))
+rotation_functions.append(lambda x, y, z: (x, -z, y))
+rotation_functions.append(lambda x, y, z: (z, x, y))
+rotation_functions.append(lambda x, y, z: (-x, z, y))
+rotation_functions.append(lambda x, y, z: (-x, y, -z))
+rotation_functions.append(lambda x, y, z: (-y, -x, -z))
+rotation_functions.append(lambda x, y, z: (x, -y, -z))
+rotation_functions.append(lambda x, y, z: (y, x, -z))
+rotation_functions.append(lambda x, y, z: (y, -z, -x))
+rotation_functions.append(lambda x, y, z: (z, y, -x))
+rotation_functions.append(lambda x, y, z: (-y, z, -x))
+rotation_functions.append(lambda x, y, z: (-z, -y, -x))
 
 def get_all_rotations(cube):
  
     all_rotations = []
 
-    for i, transformation_function in enumerate(transformation_functions):
-        cube_name = f"{cube[0]} => {i}"
-        beacons = [transformation_function(x, y, z) for (x, y, z) in cube[1]]
-        all_rotations.append((cube_name, beacons))
+    for i, rotation_function in enumerate(rotation_functions):
+        (original_cube_name, original_beacons) = cube
+        rotated_cube_name = f"{original_cube_name} => {i}" # this is only useful for debugging
+        rotated_beacons = [rotation_function(x, y, z) for (x, y, z) in original_beacons]
+        rotated_cube = (rotated_cube_name, rotated_beacons)
+        all_rotations.append(rotated_cube)
 
     return all_rotations
 
@@ -120,34 +116,36 @@ def overlap(space, beacons, offset, expected_match_count):
     else:
         return False
 
-def check_overlap(space_beacons, cube_beacons, match_count):
+def try_to_fit(space_beacons, cube_beacons, match_count):
 
     distances = {}
     
-    # print(f"check_overlap: must do { len(cube_beacons) * len(space_beacons)} checks")
+    # print(f"try_to_fit: must do { len(cube_beacons) * len(space_beacons)} checks")
 
     for cube_beacon in cube_beacons:
         for space_beacon in space_beacons:
+            # calculate the distance from the cube beacon to the space beacon
             dx = cube_beacon[0] - space_beacon[0]
             dy = cube_beacon[1] - space_beacon[1]
             dz = cube_beacon[2] - space_beacon[2]
             distance = (dx, dy, dz)
             if distance in distances:
                 distances[distance] += 1
+                # if we have enough distances which are the same, then we can count this as a fit
                 if distances[distance] >= match_count:
                     offset = (-dx, -dy, -dz)
-                    return offset
+                    return (True, offset) # (cube_fits, offset)
             else:
                 distances[distance] = 1
 
-    return None
+    return (False, None) # (cube_fits, offset)
 
-def get_max_distance(cubes, match_count):
+def build_space(cubes, match_count):
 
     space = set()
-    scanners = []
+    scanners = []    
     
-    # anchor the first tile at (0,0,0)
+    # anchor the first cube as the center of our space
     cube = cubes[0]
     cubes.remove(cube)
     offset = (0, 0, 0)
@@ -160,21 +158,22 @@ def get_max_distance(cubes, match_count):
         cubes.remove(cube)
         cube_rotations = get_all_rotations(cube)
         offset = None        
-        for cube_rotation in cube_rotations:
-            #print(f"Checking '{cube_rotation[0]}'...")
-            offset = check_overlap(space, cube_rotation[1], match_count)
-            if offset is not None:
-                add_to_space(cube_rotation, offset, space)
+        for rotated_cube in cube_rotations:
+            (rotated_cube_name, rotated_beacons) = rotated_cube
+            #print(f"Attempting to fit '{rotated_cube_name}'...")
+            (cube_fits, offset) = try_to_fit(space, rotated_beacons, match_count)
+            if cube_fits:
+                add_to_space(rotated_cube, offset, space)
                 scanners.append(offset)
                 break
-        if offset is None: # after checking all rotations, we failed to place this tile, save it for later
-            #print(f"FAILED to match '{cube[0]}' in any rotation... will try again later")
+        if offset is None: # after checking all rotations, we failed to place this cube, save it for later
+            #print(f"FAILED to fit '{cube[0]}' in any rotation... will try again later")
             cubes.append(cube)
 
-    # count the number of beacons in space
-    # return len(space)
+    return space, scanners
 
-    return calc_max_distance(scanners)
+def get_number_of_beacons(space):
+    return len(space)
 
 def calc_max_distance(scanners):
     max_distance = 0
@@ -187,10 +186,11 @@ def calc_max_distance(scanners):
     return max_distance
 
 def execute(input, match_count = 12):
-    #print(input)
-    cubes = parse_input(input) 
 
-    result = get_max_distance(cubes, match_count)
+    cubes = parse_input(input) 
+    space, scanners = build_space(cubes, match_count)
+    result = calc_max_distance(scanners)
+
     print(f"result: {result}") 
     return result
 
