3113e19967a827be7c169a38e71fc398c90efa4c
JackieMaw <jackiemaw@gmail.com>
2022-12-14 09:35:36 +0000

    leaderboards

--- python/2022/leaderboards.py
+++ python/2022/leaderboards.py
@@ -11,6 +11,7 @@ def get_leaderboard_json(year, leaderboard_code):
     return response.text
 
 def get_stars_count(leaderboard_json, goal) -> int:
+    total_active_members = 0
     total_stars = 0
     count_goal = 0
     data = json.loads(leaderboard_json)
@@ -20,29 +21,31 @@ def get_stars_count(leaderboard_json, goal) -> int:
             num_stars = members[key]["stars"]
             if num_stars >= goal:
                 count_goal += 1
+            if num_stars >= 1:
+                total_active_members += 1
             total_stars += min(goal, num_stars) 
     except Exception as exp:
         print("Exception {}".format(exp))
         return -1
-    return (total_stars, count_goal)
+    return (total_active_members, total_stars, count_goal)
 
 def process_leaderboard(leaderboard, code):
     leaderboard_json = get_leaderboard_json(YEAR, code)
-    (total_stars, count_goal) = get_stars_count(leaderboard_json, goal)
-    return (leaderboard, total_stars, count_goal)
+    (total_active_members, total_stars, count_goal) = get_stars_count(leaderboard_json, goal)
+    return (leaderboard, total_active_members, total_stars, count_goal)
 
 def sort_by_total_stars(stat):
-    (_, total_stars, _) = stat
+    (_, _, total_stars, _) = stat
     return total_stars
 
 # process leaderboards
 
 YEAR = 2022
 all_leaderboards = { "GLOBAL":"384496", "Singapore":"1580364", "India":"639163", "Poland":"392146", "Switzerland":"212737", "UK":"825756", "US":"984566" }
-goal=24
+goal=26
 
 stats = [process_leaderboard(leaderboard, code) for (leaderboard, code) in all_leaderboards.items()]
 stats.sort(reverse=True, key=sort_by_total_stars)
 
-for (leaderboard, total_stars, count_goal) in stats:
-    print(f"{leaderboard} leaderboard has {total_stars} stars, with {count_goal} participants achieving at least {goal} stars")
+for (leaderboard, total_active_members, total_stars, count_goal) in stats:
+    print(f"{leaderboard} leaderboard has {total_active_members} active members achieving {total_stars} stars, with {count_goal} participants achieving at least {goal} stars")
