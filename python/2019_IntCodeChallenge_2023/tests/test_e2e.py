7cfd2a35eebbe6b36e90edcf155d0b7ae77ca34a
JackieMaw <jackiemaw@gmail.com>
2023-06-14 12:00:51 +0100

    Day17 In Progress

--- python/2019_IntCodeChallenge_2023/tests/test_e2e.py
+++ python/2019_IntCodeChallenge_2023/tests/test_e2e.py
@@ -1,12 +1,5 @@
 import model.IntCodeComputer
 
-def execute_diagnostic(input_data, input_stream, output_stream):
-    computer = model.IntCodeComputer.IntCodeComputer(input_data, input_stream, output_stream)
-    computer.run()
-    diagnostic_code = output_stream[len(output_stream) - 1]
-    return diagnostic_code
-
-
 def test_execute_9a():
 
     with open("./input/day9_actual.txt", "r") as text_file:
@@ -14,7 +7,9 @@ def test_execute_9a():
 
     input_stream = [1]
     output_stream = []
-    result = execute_diagnostic(input_data, input_stream, output_stream)
+    computer = model.IntCodeComputer.IntCodeComputer(input_data, input_stream, output_stream)
+    computer.run()
+    result = computer.get_diagnostic_code()
 
     assert result == 2377080455
 
@@ -25,6 +20,25 @@ def test_execute_9b():
 
     input_stream = [2]
     output_stream = []
-    result = execute_diagnostic(input_data, input_stream, output_stream)
+    computer = model.IntCodeComputer.IntCodeComputer(input_data, input_stream, output_stream)
+    computer.run()
+    result = computer.get_diagnostic_code()
 
-    assert result == 2377080455
+    assert result == 74917
+
+def test_execute_17a():
+
+    with open("./input/day17_actual.txt", "r") as text_file:
+        input_data = [int(l) for l in text_file.read().split(",")]
+
+    input_stream = []
+    output_stream = []
+    computer = model.IntCodeComputer.IntCodeComputer(input_data, input_stream, output_stream)
+    computer.run()
+
+    print(to_ascii(output_stream))
+    assert output_stream == 74917
+
+def to_ascii(output_stream):
+    ascii_stream = ''.join(chr(i) for i in output_stream)
+    return ascii_stream
