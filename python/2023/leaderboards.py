4a5cfbee6f4cbca876f02d1df1e3928fbbac50d4
JackieMaw <jackiemaw@gmail.com>
2023-11-29 18:55:25 +0000

    Getting Ready for AoC 2023!

---
+++ python/2023/leaderboards.py
@@ -0,0 +1,66 @@
+import json
+import requests
+
+SESSIONID = "GET_THIS_FROM_SESSION_COOKIE"
+USER_AGENT = ""
+
+def get_leaderboard_json(year, leaderboard_code):
+    uri = f'https://adventofcode.com/{year}/leaderboard/private/view/{leaderboard_code}.json'
+    print(f"Reading input from uri: {uri}")
+    response = requests.get(uri, cookies={'session': SESSIONID}, headers={'User-Agent': USER_AGENT})
+    print(response.text)
+    return response.text
+
+def get_stars_count(leaderboard_json, goal) -> int:
+    total_members = 0
+    total_active_members = 0
+    total_stars = 0
+    count_goal = 0
+    data = json.loads(leaderboard_json)
+    members = data.get("members")
+    try:
+        for key in members.keys():
+            total_members += 1
+            num_stars = members[key]["stars"]
+            if num_stars >= goal:
+                count_goal += 1
+            if num_stars >= 1:
+                total_active_members += 1
+            total_stars += min(goal, num_stars) 
+    except Exception as exp:
+        print("Exception {}".format(exp))
+        return -1
+    return (total_members, total_active_members, total_stars, count_goal)
+
+def process_leaderboard(leaderboard, code):
+    leaderboard_json = get_leaderboard_json(YEAR, code)
+    (total_members, total_active_members, total_stars, count_goal) = get_stars_count(leaderboard_json, goal)
+    return (leaderboard, total_members, total_active_members, total_stars, count_goal)
+
+def sort_by_total_stars(stat):
+    (_, _, _, total_stars, _) = stat
+    return total_stars
+
+def print_active_members(leaderboard_json):
+    data = json.loads(leaderboard_json)
+    members = data.get("members")
+    for key in members.keys():
+        num_stars = members[key]["stars"]
+        if num_stars >= 1:
+            print(f"{members[key]['name']}; ")
+
+def print_active_members_for_leaderboard(code):
+    leaderboard_json = get_leaderboard_json(YEAR, code)
+    print_active_members(leaderboard_json)
+
+# process leaderboards
+
+YEAR = 2023
+all_leaderboards = { "APAC":"1580364", "Switzerland":"212737", "EMEA":"825756", "AMER":"2328970" }
+goal=50
+
+stats = [process_leaderboard(leaderboard, code) for (leaderboard, code) in all_leaderboards.items()]
+stats.sort(reverse=True, key=sort_by_total_stars)
+
+for (leaderboard, total_members, total_active_members, total_stars, count_goal) in stats:
+    print(f"{leaderboard} leaderboard has {total_active_members}/{total_members} active members achieving {total_stars} stars, with {count_goal} participants achieving at least {goal} stars")
---
+++ python/2023/utilities.py
@@ -0,0 +1,44 @@
+import requests
+from os.path import exists
+
+SESSIONID = "GET_THIS_FROM_SESSION_COOKIE"
+USER_AGENT = ""
+
+def get_or_download_input(year, day):
+    filename = f"input\Input_{year}_{day}.txt"
+    if not(exists(filename)):
+        download_input(year, day, filename)    
+    with open(filename, "r") as text_file:
+        return text_file.read().splitlines()
+
+def get_input(year, day, postfix):
+    filename = f"input\Input_{year}_{day}{postfix}.txt"
+    print(f"Reading input from file: {filename}")
+    with open(filename, "r") as text_file:
+        return text_file.read().splitlines()
+
+def download_input(year, day, filename):
+    uri = f'http://adventofcode.com/{year}/day/{day}/input'
+    print(f"Reading input from uri: {uri}")
+    response = requests.get(uri, cookies={'session': SESSIONID}, headers={'User-Agent': USER_AGENT})
+    print(f"Writing input to file: {filename}")
+    with open(filename, "w") as text_file:
+        text_file.write(response.text)
+
+def get_strings(raw_input):
+    return [l for l in raw_input]
+
+def get_strings_csv(raw_input):
+    result = []
+    for line in raw_input:
+        inner_result = []
+        for l in line.split(","):   
+            inner_result.append(l)
+        result.append(inner_result)
+    return result
+
+def get_integers(raw_input):
+    return [int(l) for l in raw_input]
+
+def get_integers_csv(raw_input):
+    return [int(l) for l in raw_input[0].split(",")]
