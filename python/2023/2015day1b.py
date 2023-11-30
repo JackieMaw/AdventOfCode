504d16f560bbfbd4deba8afa92eb290e4d2673f3
JackieMaw <jackiemaw@gmail.com>
2023-11-30 19:51:30 +0000

    Practicing - 2015 Day 1

--- python/2023/2015day1b.py
+++ python/2023/2015day1b.py
@@ -4,12 +4,23 @@ import copy
 
 def execute(input):
     print(input)
-    result = input.count('(') - input.count(')')
+    stack = []
+    pos = 0
+    for i in input:
+        pos += 1
+        if i == '(':
+            stack.append(i)
+        elif len(stack) == 0:
+            break
+        else:
+            stack.pop()
+    result = pos
     print(f"result: {result}")
     return result
 
 # TESTS
-assert execute(get_strings(["(())"])[0]) == 0
+assert execute(get_strings([")"])[0]) == 1
+assert execute(get_strings(["()())"])[0]) == 5
 print("ALL TESTS PASSED")
 
 YEAR = 2015
@@ -24,5 +35,5 @@ DAY = 1
 # REAL INPUT DATA
 raw_input = get_or_download_input(YEAR, DAY)
 input = get_strings(raw_input)
-assert execute(input[0]) == 280
+assert execute(input[0]) == 0
 print("ANSWER CORRECT")
