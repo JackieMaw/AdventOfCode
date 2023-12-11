40ec7370d311bfc0cee5c212627cb60a047335ba
JackieMaw <jackiemaw@gmail.com>
2023-12-11 21:39:28 +0000

    2023 Day 10a finally completed

--- python/2023/2023day10a.py
+++ python/2023/2023day10a.py
@@ -1,5 +1,6 @@
 import heapq
 from itertools import count
+import itertools
 from utilities import *
 import math
 import copy
@@ -20,6 +21,12 @@ neighbour_rules = {'|' : [north, south],
               '.' : [],
               'S' : [north, east, south, west]}
 
+compatability_rules = { north : ['|', '7', 'F'],
+                       east : ['-', '7', 'J'],
+                       south : ['|', 'J', 'L'],
+                       west : ['-', 'L', 'F'],
+                        }
+
 def get_starting_position(input_lines):
     for line_number, input_line in enumerate(input_lines):
         col_number = input_line.find('S')
@@ -39,51 +46,62 @@ def is_valid_location(location):
     return True
 
 
-def get_neighbours(current_location, char, visited):    
+def get_neighbours(current_location, grid, visited):    
     (row, col) = current_location
-    possible_neighbours = [(row + row_offset, col + col_offset) for (row_offset, col_offset) in neighbour_rules[char]]
-    not_visited_yet = [possible_neighbour for possible_neighbour in possible_neighbours if is_valid_location(possible_neighbour) and possible_neighbour not in visited]
+    char = grid[row][col]
+    possible_neighbours = []
+    for possible_neighbour in neighbour_rules[char]:
+        (row_offset, col_offset) = possible_neighbour
+        new_row = row + row_offset
+        new_col = col + col_offset
+        new_location = (new_row, new_col)
+        if is_valid_location(new_location):
+            new_char = grid[new_row][new_col]
+            if new_char in compatability_rules[possible_neighbour]:
+                possible_neighbours.append(new_location)
+    
+    not_visited_yet = [possible_neighbour for possible_neighbour in possible_neighbours if possible_neighbour not in visited]
     return not_visited_yet
 
 def display_matrix(matrix):
     for row in matrix:
-        print(' '.join([ str(col) for col in row ]))
+        print(' '.join([str(col) for col in row]))
 
 #Djikstra's Algorithm - Shortest Path to All Nodes
 def get_all_distances(grid, starting_position):
 
     global num_rows
-    num_rows = len(grid[0])
+    num_rows = len(grid)
     global num_cols
-    num_cols = len(grid)
+    num_cols = len(grid[0])
 
-    distance_grid = [ [ 0 for _ in range(num_rows) ] for _ in range(num_cols) ]
-    visited = [ [ 0 for _ in range(num_rows) ] for _ in range(num_cols) ]
+    distance_grid = [ [ 'X' for _ in range(num_cols) ] for _ in range(num_rows) ]
+    already_visited = [ [ 0 for _ in range(num_cols) ] for _ in range(num_rows) ]
 
     (row_start, col_start) = starting_position
-    priority_queue = [(0, row_start, col_start)] # (cost, row, col)
+    nodes_to_visit_priority_queue = [(0, starting_position)] # (cost, (row, col))
     distance_grid[row_start][col_start] = 0
-    visited[row_start][col_start] = 1
+    already_visited[row_start][col_start] = 1
 
     display_matrix(grid)
     #display_matrix(distance_grid)
     #display_matrix(visited)
 
-    while len(priority_queue) > 0:
-        (distance_to_here, row, col) = heapq.heappop(priority_queue)
-        visited[row][col] = 1
-        #print(f"Visiting: {(row, col)}")
-        neighbours = get_neighbours((row, col), grid[row][col], visited)
+    while len(nodes_to_visit_priority_queue) > 0:
+        (distance_to_here, (row, col)) = heapq.heappop(nodes_to_visit_priority_queue)
+        already_visited[row][col] = 1
+        print(f"Visiting: {(row, col)}")
+        neighbours = get_neighbours((row, col), grid, already_visited)
         for (nrow, ncol) in neighbours:
-            if grid[nrow][ncol] != '.':
-                distance_to_neighbour = distance_grid[nrow][ncol]
-                new_distance_to_neighbour = distance_to_here + 1
-                if distance_to_neighbour == 0  or new_distance_to_neighbour < distance_to_neighbour:
-                    #print(f"    found shortest path to {(nrow, ncol)} : {new_distance_to_neighbour}")
-                    distance_grid[nrow][ncol] = new_distance_to_neighbour
-                    # do not add if already visited
-                    if not visited[nrow][ncol]:
-                        heapq.heappush(priority_queue, (new_distance_to_neighbour, nrow, ncol))
+            print(f"  >> {(nrow, ncol)}")
+            distance_to_neighbour = distance_grid[nrow][ncol]
+            new_distance_to_neighbour = distance_to_here + 1
+            if distance_to_neighbour == 'X' or new_distance_to_neighbour < distance_to_neighbour:
+                print(f"    found shortest path to {(nrow, ncol)} : {new_distance_to_neighbour}")
+                distance_grid[nrow][ncol] = new_distance_to_neighbour
+                # do not add if already visited
+                if not already_visited[nrow][ncol]:
+                    heapq.heappush(nodes_to_visit_priority_queue, (new_distance_to_neighbour, (nrow, ncol)))
 
 
     display_matrix(distance_grid)
@@ -92,24 +110,17 @@ def get_all_distances(grid, starting_position):
     return distance_grid
 
 
-def get_max_turning_point(distance_grid):
-    
-    # for row in range(num_rows):
-    #     for col in range(num_cols):
-    #         distance = distance_grid[row][col]
-    #         ## if two neighbours have distance - 1
-    #         num_neighbours_less_one = sum(1 for (nrow, ncol) in get_neighbours() if distance_grid[nrow][ncol] == distance - 1)
-    #         if num_neighbours_less_one == 2:
-    #             return distance
-            
-    raise Exception("No Max Turning Point Found")
+def is_not_none(x):
+    return x != 'X'
 
+def get_max_distance(distance_grid):    
+    all_distances = itertools.chain(*[list(filter(is_not_none, row)) for row in distance_grid])
+    return max(all_distances)
 
 def get_safest_distance_from_animal(input_lines):
     starting_position = get_starting_position(input_lines)
     distance_grid = get_all_distances(input_lines, starting_position)
-    return get_max_turning_point(distance_grid)
-    #return max([max(row) for row in distance_grid])
+    return get_max_distance(distance_grid)
 
 def execute(input_lines):
     print(input_lines)
@@ -137,10 +148,13 @@ assert execute(input) == 8
 raw_input = get_input(YEAR, DAY, "_test4")
 input = get_strings(raw_input)
 assert execute(input) == 8
+raw_input = get_input(YEAR, DAY, "_test5")
+input = get_strings(raw_input)
+assert execute(input) == 5
 print("TEST INPUT PASSED")
 
 # REAL INPUT DATA
 raw_input = get_or_download_input(YEAR, DAY)
 input_lines = get_strings(raw_input)
-assert execute(input_lines) == 0
+assert execute(input_lines) == 6897
 print("ANSWER CORRECT")
