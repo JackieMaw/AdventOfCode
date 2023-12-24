4dcb265b8cfcec623516409c053b70f6981e3cee
JackieMaw <jackiemaw@gmail.com>
2023-12-24 17:10:25 +0200

    Refactoring Leaderboards

--- python/2023/leaderboards.py
+++ python/2023/leaderboards.py
@@ -1,11 +1,30 @@
 import json
 import requests
 
+def remove_polish_characters(text):
+    # Dictionary mapping Polish characters to English equivalents
+    polish_chars = {
+        'ą': 'a', 'ć': 'c', 'ę': 'e', 'ł': 'l', 'ń': 'n', 'ó': 'o',
+        'ś': 's', 'ź': 'z', 'ż': 'z', 'Ą': 'A', 'Ć': 'C', 'Ę': 'E',
+        'Ł': 'L', 'Ń': 'N', 'Ó': 'O', 'Ś': 'S', 'Ź': 'Z', 'Ż': 'Z'
+    }
+    
+    # Replace Polish characters with their English equivalents
+    for polish_char, english_char in polish_chars.items():
+        text = text.replace(polish_char, english_char)
+    
+    return text
+
 class MemberSummary():
 
     def __init__(self, member_info):
-        self.name = member_info["name"]
+        self.name = member_info["name"]        
         self.id = member_info["id"]
+        if self.name is None:
+            #self.name = f"(anonymous user #{self.id})"
+            self.name = f"{self.id}"
+        else:
+            self.name = remove_polish_characters(self.name)
         self.stars = member_info["stars"]
         self.local_score = member_info["local_score"]
         self.last_star_ts = member_info["last_star_ts"]
@@ -26,24 +45,35 @@ class LeaderboardSummary():
         return sum(1 for member_summary in self.member_summaries if member_summary.stars > 0)
     
     def get_count_members_achieved_goal(self, goal):
-        return sum(1 for member_summary in self.member_summaries if member_summary.stars > goal)
+        return sum(1 for member_summary in self.member_summaries if member_summary.stars >= goal)
     
+    def export_to_csv(self, filename):
+        print(f"Writing leaderboard to file: {filename}")
+        self.member_summaries.sort(reverse=True, key=sort_by_local_score)
+        with open(filename, "a", encoding="utf-8") as text_file:
+            for index, member_summary in enumerate(self.member_summaries):
+                text_file.write(f'{self.leaderboard_name},{index+1},{member_summary.name},{member_summary.stars}\n')
+
     def print_summary(self, goal):
         print(f"{self.leaderboard_name} leaderboard has {self.get_count_active_members()}/{self.get_count_members()} active members achieving {self.get_total_stars()} stars, with {self.get_count_members_achieved_goal(goal)} participants achieving at least {goal} stars")
         print("TOP 10")
         self.member_summaries.sort(reverse=True, key=sort_by_local_score)
-        for member_summary in self.member_summaries[:20]:
-            print(f'    {member_summary.name} {member_summary.stars} {member_summary.local_score}')
+        for index, member_summary in enumerate(self.member_summaries[:10]):
+            print(f'{index+1}. {member_summary.name} {member_summary.stars}*')
+            #print(f'{index+1}. {member_summary.name} {member_summary.stars}* ==> {member_summary.local_score}')
 
 def get_session_id():
     with open("session_id_ubs_admin.txt", "r") as text_file:
         return text_file.read()
 
-def get_leaderboard_json(year, leaderboard_code):
+def get_leaderboard_json(year, name, leaderboard_code):
     uri = f'https://adventofcode.com/{year}/leaderboard/private/view/{leaderboard_code}.json'
     print(f"Reading input from uri: {uri}")
     response = requests.get(uri, cookies={'session': get_session_id()})
-    print(response.text)
+    #print(response.text)    
+    filename = f"leaderboards\{name}_{year}_{leaderboard_code}.json"
+    with open(filename, "w", encoding="utf-8") as text_file:
+        text_file.write(response.text)
     return response.text
 
 def get_leaderboard_summary(leaderboard_name, leaderboard_json) -> int:
@@ -53,7 +83,7 @@ def get_leaderboard_summary(leaderboard_name, leaderboard_json) -> int:
     return LeaderboardSummary(leaderboard_name, member_summaries)
 
 def extract_leaderboard_summary(leaderboard_name, code):
-    leaderboard_json = get_leaderboard_json(YEAR, code)
+    leaderboard_json = get_leaderboard_json(YEAR, leaderboard_name, code)
     return get_leaderboard_summary(leaderboard_name, leaderboard_json)
 
 def sort_by_local_score(member_summary : MemberSummary):
@@ -89,7 +119,7 @@ def get_sum(leaderboard_summaries):
 
 YEAR = 2023
 all_leaderboards = { "APAC":"1580364", "Switzerland":"212737", "EMEA":"825756", "AMER":"2328970", "GLOBAL_TOP_100":"2087277" }
-goal=28
+goal=34
 IGNORED_MEMBERS = {"UBS Admin"}
 
 leaderboard_summaries = [extract_leaderboard_summary(leaderboard, code) for (leaderboard, code) in all_leaderboards.items()]
@@ -97,5 +127,10 @@ leaderboard_summaries.sort(reverse=True, key=sort_by_total_stars)
 
 #leaderboard_summaries.append(get_sum(leaderboard_summaries))
 
+filename = f"leaderboards\All_Leaderboards.csv"
+with open(filename, "w", encoding="utf-8") as text_file:
+    text_file.write('Leaderboard,Rank,Name,Stars\n')
+
 for leaderboard_summary in leaderboard_summaries:
     leaderboard_summary.print_summary(goal)
+    leaderboard_summary.export_to_csv(filename)
