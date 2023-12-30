3559e2aabfa62878e9e7d0ebe39f671c6773c769
JackieMaw <jackiemaw@gmail.com>
2023-12-30 19:36:17 +0200

    AoC 2023 Day 14b Completed!

--- python/2023/2023day14b.py
+++ python/2023/2023day14b.py
@@ -22,7 +22,7 @@ def move_next_marble_north(grid, row_num, col_num):
     for r in range(row_num + 1, len(grid)):
         char = grid[r][col_num]
         if char == "O":
-            print(f"moved north from ({r, col_num}) to ({row_num, col_num})")
+            #print(f"moved north from ({r, col_num}) to ({row_num, col_num})")
             grid[row_num][col_num] = "O"
             grid[r][col_num] = "."
             return
@@ -34,7 +34,7 @@ def move_next_marble_south(grid, row_num, col_num):
     for r in range(row_num - 1, -1, -1):
         char = grid[r][col_num]
         if char == "O":
-            print(f"moved south from ({r, col_num}) to ({row_num, col_num})")
+            #print(f"moved south from ({r, col_num}) to ({row_num, col_num})")
             grid[row_num][col_num] = "O"
             grid[r][col_num] = "."
             return
@@ -46,7 +46,7 @@ def move_next_marble_west(grid, row_num, col_num):
     for c in range(col_num + 1, len(grid[0])):
         char = grid[row_num][c]
         if char == "O":
-            print(f"moved west from ({row_num, c}) to ({row_num, col_num})")
+            #print(f"moved west from ({row_num, c}) to ({row_num, col_num})")
             grid[row_num][col_num] = "O"
             grid[row_num][c] = "."
             return
@@ -58,7 +58,7 @@ def move_next_marble_east(grid, row_num, col_num):
     for c in range(col_num - 1, -1, -1):
         char = grid[row_num][c]
         if char == "O":
-            print(f"moved east from ({row_num, c}) to ({row_num, col_num})")
+            #print(f"moved east from ({row_num, c}) to ({row_num, col_num})")
             grid[row_num][col_num] = "O"
             grid[row_num][c] = "."
             return
@@ -121,13 +121,48 @@ def spin(grid):
     roll_east(grid)
     #print("AFTER ROLL EAST")
     #display_matrix(grid)
+    
+    result = get_total_load(grid)
 
-    if get_hash(grid) == grid_hash_before:
-        raise Exception("No changes!")
+    spin_cache[grid_hash_before] = (copy.deepcopy(grid), result)
 
-    spin_cache[grid_hash_before] = copy.deepcopy(grid)
+    return (grid, result)
 
-    return grid
+def is_repeating_sequence(all_results, repeating_sequence_start, repeating_sequence_length):
+    #repeating sequence must repeat all the way to the end
+
+    result = False
+    s1 = repeating_sequence_start
+    e1 = s1 + repeating_sequence_length
+    sequence = all_results[s1:e1]
+    s2 = e1
+    e2 = s2 + repeating_sequence_length
+    while e2 < len(all_results):
+        if all_results[s2:e2] != sequence:
+            return False
+        else:
+            result = True
+            s2 = e2
+            e2 = s2 + repeating_sequence_length
+
+    return result
+
+def find_repeating_sequence(all_results):
+
+    for repeating_sequence_length in range(2, len(all_results)//2):
+        for repeating_sequence_start in range(0, len(all_results)-repeating_sequence_length):
+            if is_repeating_sequence(all_results, repeating_sequence_start, repeating_sequence_length):
+                print(f"Found Repeating Sequence at {repeating_sequence_start} of length {repeating_sequence_length}")
+                sequence = all_results[repeating_sequence_start:repeating_sequence_start +repeating_sequence_length]
+                print(sequence)
+                return repeating_sequence_start, repeating_sequence_length, sequence
+
+    raise Exception("Couldn't find repeating sequence.")
+
+def extrapolate_results(all_results, extrapolation_limit):
+    repeating_sequence_start, repeating_sequence_length, sequence = find_repeating_sequence(all_results)
+    index = (extrapolation_limit - repeating_sequence_start) % repeating_sequence_length - 1
+    return sequence[index]
 
 def execute(input_lines):
     
@@ -135,19 +170,16 @@ def execute(input_lines):
     grid = [ list(line) for line in input_lines ]
     #print(grid)
     
-    #for _ in range(100):
-    for i in range(1000000000):
-    #for _ in range(1):
-        grid = spin(grid)
-        if i % 10000 == 0:
-            print(f"ITERATION {i}")
-            result = get_total_load(grid)
-            print(f"result: {result}")
+    all_results = []
+    for i in range(1000):
+        (grid, result) = spin(grid)
+        all_results.append(result)
+        #print(f"ITERATION {i} result: {result}")
 
-    result = get_total_load(grid)
-    print(f"result: {result}")
+    result = extrapolate_results(all_results, 1000000000)
+    print(result)
     return result
-
+ 
 # TESTS
 test_grid = [['.', '.', '.'], ['.', 'O', '.'], ['.', '.', '.']]
 roll_north(test_grid)
@@ -161,6 +193,13 @@ assert test_grid == [['.', '.', '.'], ['O', '.', '.'], ['.', '.', '.']]
 test_grid = [['.', '.', '.'], ['.', 'O', '.'], ['.', '.', '.']]
 roll_east(test_grid)
 assert test_grid == [['.', '.', '.'], ['.', '.', 'O'], ['.', '.', '.']]
+
+# TESTS for Repeating Sequence
+
+assert is_repeating_sequence([1, 2, 3, 4, 2, 3, 4, 2, 3], 1, 3) == True
+assert is_repeating_sequence([1, 2, 3, 4, 2, 3, 5, 2, 3], 1, 3) == False
+assert find_repeating_sequence([1, 2, 3, 4, 2, 3, 4, 2, 3]) == (1, 3, [2, 3, 4])
+
 print("UNIT TESTS PASSED")
 
 YEAR = 2023
@@ -175,5 +214,5 @@ print("TEST INPUT PASSED")
 # REAL INPUT DATA
 raw_input = get_or_download_input(YEAR, DAY)
 input_lines = get_strings(raw_input)
-assert execute(input_lines) == 0
+assert execute(input_lines) == 99291
 print("ANSWER CORRECT")
