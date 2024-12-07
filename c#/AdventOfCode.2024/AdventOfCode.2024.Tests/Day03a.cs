17718a448a20ac49876d8692ca6e9172b0831b01
JackieMaw <jackiemaw@gmail.com>
2024-12-07 21:49:48 +0000

    Day 3a Completed

---
+++ c#/AdventOfCode.2024/AdventOfCode.2024.Tests/Day03a.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.RegularExpressions;
+using AdventOfCode._2023;
+
+namespace AdventOfCode._2024.Tests;
+
+public class Day03a
+{
+    private const int day = 3;
+    private const int year = 2024;
+    private IAoCSupplier aocSupplier = new AoCFilesSupplier();
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void UnitTests()
+    {
+        var result = 0;
+        var expectedResult = 0;
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }    
+
+    [Test]
+    public void TestSampleInput()
+    {
+        Console.WriteLine("Testing Sample Input...");
+        var expectedResult = 161;
+        var input = aocSupplier.GetPuzzleInput(year, day, "_test");
+        var result = Execute(input);
+        Console.WriteLine($"Result: {result}");
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void TestFullInput()
+    {
+        Console.WriteLine("Testing Full Input...");
+        var expectedResult = 189527826;
+        var input = aocSupplier.GetPuzzleInput(year, day);
+        var result = Execute(input);
+        Console.WriteLine($"Result: {result}");
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    private long Execute(string[] input)
+    {
+        long result = 0;
+
+        string pattern = @"mul\((\d+),(\d+)\)";
+
+        Regex regex = new Regex(pattern);
+
+        foreach (var inputLine in input)
+        {
+            Match match = regex.Match(inputLine);
+
+            while (match.Success)
+            {
+                var x = Convert.ToInt32(match.Groups[1].Value);
+                var y = Convert.ToInt32(match.Groups[2].Value);
+                result += x * y;
+
+                Console.WriteLine("Found match: " + match.Value);
+                match = match.NextMatch();
+            }
+        }
+
+        return result;
+    }
+}
