1821d8d9ca8dc3046570753c94c87370541a472e
JackieMaw <jackiemaw@gmail.com>
2024-12-16 06:51:10 +0000

    Day 16a In Progress

---
+++ c#/AdventOfCode.2024/AdventOfCode.2024.Tests/Day16a.cs
@@ -0,0 +1,162 @@
+﻿using AdventOfCode._2023;
+
+namespace AdventOfCode._2024.Tests;
+
+public class Day16a
+{
+    private const int day = 16;
+    private const int year = 2024;
+    private IAoCSupplier aocSupplier = new AoCFilesSupplier();
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void UnitTest_CheckThatPriorityQueueBehaves()
+    {
+        var thingToEnQ = new DirectedPosition(new Point(5, 5), Point.Right);
+        var priorityQueue = new PriorityQueue<DirectedPosition, long>();
+        priorityQueue.Enqueue(thingToEnQ, 0);
+
+        var thingDeQ = priorityQueue.Dequeue();
+
+        Assert.That(thingDeQ, Is.EqualTo(thingToEnQ));
+    }    
+
+    [Test]
+    public void TestSampleInput_1()
+    {
+        Console.WriteLine("Testing Sample Input 1...");
+        var expectedResult = 7036;
+        var input = aocSupplier.GetPuzzleInput(year, day, "_test1");
+        var result = Execute(input);
+        Console.WriteLine($"Result: {result}");
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void TestSampleInput_2()
+    {
+        Console.WriteLine("Testing Sample Input 2...");
+        var expectedResult = 11048;
+        var input = aocSupplier.GetPuzzleInput(year, day, "_test2");
+        var result = Execute(input);
+        Console.WriteLine($"Result: {result}");
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void TestFullInput()
+    {
+        Console.WriteLine("Testing Full Input...");
+        var expectedResult = 0;
+        var input = aocSupplier.GetPuzzleInput(year, day);
+        var result = Execute(input);
+        Console.WriteLine($"Result: {result}");
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }    
+
+    private readonly record struct DirectedPosition(Point Position, Point Direction)
+    {
+    }
+
+    private long Execute(string[] input)
+    {
+        var map = Map.CreateMap(input);
+
+        HashSet<DirectedPosition> alreadyExplored = [];
+        var notExploredYet = new PriorityQueue<DirectedPosition, long>();
+        var shortestPaths = new Dictionary<DirectedPosition, long>();
+        long? shortestPathToEnd = null;
+
+        Console.WriteLine($"Starting at {map.start} > {Point.Right}");
+        var startingState = new DirectedPosition(map.start, Point.Right);
+        notExploredYet.Enqueue(startingState, 0);
+        shortestPaths.Add(startingState, 0);
+
+        while(notExploredYet.Count > 0)
+        {
+            var exploreMeNow = notExploredYet.Dequeue();
+
+            Explore(exploreMeNow, map, notExploredYet, shortestPaths, alreadyExplored);
+
+            alreadyExplored.Add(exploreMeNow);
+        }
+        
+        if (shortestPathToEnd.HasValue)
+            return shortestPathToEnd.Value;
+        else
+            throw new Exception("No path found :-( Methinks something went wrong!");
+    }
+
+    private void Explore(DirectedPosition currentPosition, Map map, PriorityQueue<DirectedPosition, long> notExploredYet, Dictionary<DirectedPosition, long> shortestPaths, HashSet<DirectedPosition> alreadyExplored)
+    {
+        Console.WriteLine($"Exploring {currentPosition.Position} > {currentPosition.Direction}");
+
+        var possibleMoves = GetPossibleMoves(currentPosition, map);
+
+        foreach (var (newPosition, costToMove) in possibleMoves)
+        {
+            var costToGetHere = shortestPaths[currentPosition];
+            var newCost = costToGetHere + costToMove;
+            
+            if (shortestPaths.ContainsKey(newPosition))
+            {
+                //if we have found this node before, then we need to check if this new path is shorter
+                var currentCost = shortestPaths[newPosition];
+                if (newCost < currentCost)
+                {
+                    shortestPaths[newPosition] = newCost;
+                }
+            }
+            else
+            {
+                //if this is the first time we have found this node then this is the shortest path
+                shortestPaths[newPosition] = newCost;
+            }
+
+            if (!alreadyExplored.Contains(newPosition))
+            {
+                notExploredYet.Enqueue(newPosition, newCost);
+            }            
+        }
+    }
+
+    private List<(DirectedPosition, long)> GetPossibleMoves(DirectedPosition currentPosition, Map map)
+    {  
+        var possibleMoves = new List<(DirectedPosition, long)>();
+
+        var direction = currentPosition.Direction;
+
+        var nextPosition = map.Move(currentPosition.Position, direction);
+        if (nextPosition.HasValue)
+        {
+            possibleMoves.Add((new DirectedPosition(nextPosition.Value, direction), 1));
+        }
+
+        direction = Map.TurnRight(direction); //90 degrees clockwise = 1000 cost
+        nextPosition = map.Move(currentPosition.Position, direction);
+        if (nextPosition.HasValue)
+        {
+            possibleMoves.Add((new DirectedPosition(nextPosition.Value, direction), 1001));
+        }
+
+        direction = Map.TurnRight(direction); //180 degrees clockwise = 2000 cost
+        nextPosition = map.Move(currentPosition.Position, direction);
+        if (nextPosition.HasValue)
+        {
+            possibleMoves.Add((new DirectedPosition(nextPosition.Value, direction), 2001));
+        }
+
+        direction = Map.TurnRight(direction); //180 degrees counter-clockwise = 1000 cost
+        nextPosition = map.Move(currentPosition.Position, direction);
+        if (nextPosition.HasValue)
+        {
+            possibleMoves.Add((new DirectedPosition(nextPosition.Value, direction), 1001));
+        }
+
+        return possibleMoves;
+    }    
+}
---
+++ c#/AdventOfCode.2024/AdventOfCode.2024.Tests/Map.cs
@@ -0,0 +1,102 @@
+namespace AdventOfCode._2024.Tests;
+public class Map
+{
+    HashSet<Point> obstacles;
+    public Point start;
+    public Point? end;
+    int maxX;
+    int maxY;
+
+    public Map(HashSet<Point> obstacles, Point start, Point? end, int maxX, int maxY)
+    {
+        this.obstacles = obstacles;
+        this.start = start;
+        this.end = end;
+        this.maxX = maxX;
+        this.maxY = maxY;
+    }
+ 
+    public static Map CreateMap(string[] input)
+    {
+        HashSet<Point> obstacles = [];
+        Point? start = null;
+        Point? end = null;
+
+        int maxY = input.Length;
+        int maxX = input[0].Length;
+
+        for (int y = 0; y < input.Length; y++)
+        {
+            var inputLine = input[y];
+            for (int x = 0; x < inputLine.Length; x++)
+            {
+                switch (inputLine[x])
+                {
+                    case '#': obstacles.Add(new Point(x, y));
+                    break;
+                    
+                    case 'S': start = new Point(x, y);
+                    break;
+                    
+                    case 'E': end = new Point(x, y);
+                    break;
+
+                    case '.': break;
+
+                    default: throw new Exception($"Unexpected Input: {inputLine[x]}");
+                }
+            }
+        }
+
+        if (!start.HasValue)
+        {
+            throw new Exception("Didn't find a starting position");
+        }
+
+        if (!end.HasValue)
+        {
+            throw new Exception("Didn't find an ending position");
+        }
+
+        return new Map(obstacles, start.Value, end, maxX, maxY);
+    }
+
+    public static Point TurnRight(Point direction)
+    {
+        if (direction == Point.Up) return Point.Right;
+        if (direction == Point.Right) return Point.Down;
+        if (direction == Point.Down) return Point.Left;
+        if (direction == Point.Left) return Point.Up;
+        throw new Exception($"Unexpected Direction: {direction}");
+    }
+
+    public Point? Move(Point position, Point direction)
+    {
+        var possibleMove = new Point(position.X + direction.X, position.Y + direction.Y);
+        if (IsPossibleMove(possibleMove)) return possibleMove;
+        else return null;
+    }
+
+    private bool IsPossibleMove(Point possibleMove)
+    {
+        return IsWithinRange(possibleMove) && !IsObstacle(possibleMove);
+    }
+
+    private bool IsObstacle(Point possibleMove)
+    {
+        return obstacles.Contains(possibleMove);
+    }
+
+    private bool IsWithinRange(Point currentPosition)
+    {
+        return (currentPosition.X >= 0) && (currentPosition.Y >= 0) && (currentPosition.X < maxX) && (currentPosition.Y < maxY);
+    }    
+}
+
+public readonly record struct Point(double X, double Y)
+{
+    public static Point Up = new(0, -1);
+    public static Point Right = new(1, 0);
+    public static Point Down = new(0, 1);
+    public static Point Left = new(-1, 0);
+}
