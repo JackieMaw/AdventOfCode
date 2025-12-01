c0f7109553dd4f2b67a13270fc4bd4dd1b8735d8
JackieMaw <jackiemaw@gmail.com>
2025-12-01 19:37:30 +0000

    AoC 2025 - Day 1a

--- c#/AdventOfCode.2025/AdventOfCode.2025/Day01a.cs
+++ c#/AdventOfCode.2025/AdventOfCode.2025/Day01a.cs
@@ -11,18 +11,30 @@ public class Day01a
     }
 
     [Test]
+    public void CanTurnLeftToZero()
+    {        
+        Assert.That(Execute(["L50"]), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void CanTurnRightToZero()
+    {        
+        Assert.That(Execute(["R50"]), Is.EqualTo(1));
+    }
+
+    [Test]
     public void UnitTests()
     {
         var result = 0;
         var expectedResult = 0;
         Assert.That(result, Is.EqualTo(expectedResult));
-    }    
+    } 
 
     [Test]
     public void TestSampleInput()
     {
         Console.WriteLine("Testing Sample Input...");
-        var expectedResult = 0;
+        var expectedResult = 3;
         var input = aocSupplier.GetPuzzleInput(year, day, "_sample");
         var result = Execute(input);
         Console.WriteLine($"Result: {result}");
@@ -42,6 +54,38 @@ public class Day01a
 
     private long Execute(string[] input)
     {
-        return 0;
+        int countTimesReturnedToZero = 0;
+
+        int dialPosition = 50;
+        Console.WriteLine("The dial starts by pointing at 50.");
+
+        foreach (var line in input)
+        {
+            var direction = line[0];
+            var magnitude = Convert.ToInt32(line[1..]);
+
+            switch (direction)
+            {
+                case 'L':
+                    // turn left
+                    dialPosition = (dialPosition - magnitude) % 100;
+                    break;
+                case 'R':
+                    // turn right
+                    dialPosition = (dialPosition + magnitude) % 100;
+                    break;
+                default:
+                    throw new Exception($"Unknown direction: {direction}");
+            }
+
+            Console.WriteLine($"The dial is rotated {line} to point at {dialPosition}.");
+
+            if (dialPosition == 0)
+            {
+                countTimesReturnedToZero++;
+            }
+        }
+
+        return countTimesReturnedToZero;
     }
 }
---
+++ c#/AdventOfCode.2025/AdventOfCode.Utilities/InputAssistant.cs
@@ -0,0 +1,63 @@
+﻿public class InputAssistant
+{
+    public static int[] GetInts(string filename)
+    {
+        string[] lines = System.IO.File.ReadAllLines(filename);
+        return lines.Select(l => Convert.ToInt32(l)).ToArray();
+    }
+
+    public static int[] GetIntsFromSingleLine(string inputLine)
+    {
+        return inputLine.Split(',').Select(l => Convert.ToInt32(l)).ToArray();
+    }
+
+    public static long[] GetLongsFromSingleLine(string inputLine)
+    {
+        return inputLine.Split(',').Select(l => Convert.ToInt64(l)).ToArray();
+    }
+
+    public static char[,] GetCharGrid(string[] lines)
+    {
+        var inputs = lines.Select(l => l.ToCharArray()).ToArray();
+
+        char[,] charArray = new char[lines.Length, lines[0].Length];
+
+        for (int x = 0; x < lines.Length; x++)
+        {
+            for (int y = 0; y < lines[0].Length; y++)
+            {
+                charArray[x, y] = inputs[x][y];
+            }
+        }
+
+        return charArray;
+    }
+
+    public static int[,] GetIntGrid(string[] lines)
+    {
+        var inputs = lines.Select(l => l.ToCharArray()).ToArray();
+
+        int[,] array = new int[lines[0].Length, lines.Length];
+
+        for (int x = 0; x < lines.Length; x++)
+        {
+            for (int y = 0; y < lines[0].Length; y++)
+            {
+                array[x, y] = inputs[y][x] - '0';
+            }
+        }
+
+        return array;
+    }
+
+    public static long[] GetLongs(string filename)
+    {
+        string[] lines = System.IO.File.ReadAllLines(filename);
+        return lines.Select(l => Convert.ToInt64(l)).ToArray();
+    }
+
+    public static string[] GetStrings(string filename)
+    {
+        return System.IO.File.ReadAllLines(filename);
+    }
+}
