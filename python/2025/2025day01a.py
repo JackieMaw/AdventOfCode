709abe89a14b2e8c8113da7f16456883ef1d6abb
JackieMaw <jackiemaw@gmail.com>
2026-07-16 16:25:47 +0200

    AoC 2025 Python

---
+++ python/2025/2025day01a.py
@@ -0,0 +1,30 @@
+from utilities import *
+import math
+import copy
+
+YEAR = 2025
+DAY = 1
+
+def execute(input):
+    print(input)
+    result = 0
+    print(f"result: {result}") 
+    return result
+
+# TESTS
+# assert execute(get_strings_csv(["ABCD"])) == 0
+# print("ALL TESTS PASSED")
+
+
+
+# TEST INPUT DATA
+# raw_input = get_input(YEAR, DAY, "_test")
+# input = get_strings(raw_input)
+# assert execute(input) == 0
+# print("TEST INPUT PASSED")
+
+# REAL INPUT DATA
+raw_input = get_or_download_input(YEAR, DAY)
+input = get_strings(raw_input)
+assert execute(input) == 0
+print("ANSWER CORRECT")
